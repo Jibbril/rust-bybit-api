3409de6010015b482fb15005b6648719390c9014
Jibbril Berbres <jibbril.berbres@viego.se>
2024-03-17 17:04:01 +0100

    Setup cargo dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+
+#[actix::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    println!("Hello World!");
+
+    Ok(())
 }
